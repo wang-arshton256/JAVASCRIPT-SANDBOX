cfc73fcee4bda62e05f62bc02065480a1b48bb38
wang-arshton256 <wangarshton@gmail.com>
2025-12-23 17:14:58 +0300

    console

---
+++ 01-console/script.js
@@ -0,0 +1,7 @@
+console.log(100);
+console.error('alert');
+console.log(20, 'Hello wang', true);
+console.table({ name: 'Wang', email: 'wangarshton@gmail.com' });
+console.group('simple');
+console.log('warning');
+const styles = 'padding: 10px; background-colour: white; colour: green';
