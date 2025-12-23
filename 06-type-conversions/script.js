1fece181b702ea4a64847afaf089723327d04de9
wang-arshton256 <wangarshton@gmail.com>
2025-12-23 17:34:40 +0300

    type conversions

---
+++ 06-type-conversions/script.js
@@ -0,0 +1,11 @@
+let amount = '10000';
+
+//Change string to number
+//amount = parseInt(amount)
+
+amount = parseInt('amount');
+
+//Change number to string 
+amount = amount.toString
+ 
+console.log(amount, typeof amount);
