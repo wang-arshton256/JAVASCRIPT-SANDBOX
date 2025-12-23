21bae9503cd8995f35fa2e94030a9d5e783a08cc
wang-arshton256 <wangarshton@gmail.com>
2025-12-23 17:37:30 +0300

    numbers

---
+++ 11-numbers/script.js
@@ -0,0 +1,10 @@
+let x;
+const num = new Number(5);
+x = num.toString();
+x = num.toString().length;
+x = num.toFixed(2);
+x = num.toPrecision(3);
+x = num.toExponential(2);
+x = num.toLocaleString('en-us');
+x = num.valueOf();
+console.log(x);
