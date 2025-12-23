f3f482e69e558f2fdb6ae61c9fddca8f3995c7dc
wang-arshton256 <wangarshton@gmail.com>
2025-12-23 17:36:28 +0300

    strings

---
+++ 09-strings/script.js
@@ -0,0 +1,22 @@
+let x;
+const name = "Wangutusi";
+const age = 30;
+const address = "Bweyogerere";
+
+//Template Literals
+x = `My name is ${name},I am ${age} years old. I live at ${address}.` 
+console.log(x);
+
+//String Properties and methods
+const s = 'Hello World';
+
+x = typeof s; // To check what datatype variable (S) is of.
+X = s.length // To check the length of a string.
+X = s.toLowerCase();
+X = s.toUpperCase();
+X = s.trim();
+X = s.split(); // To change string into an array.
+x = s.indexOf(W) // Gives you the index of W
+x = s.replace() // Replaces values in the string.
+
+console.log(x);
