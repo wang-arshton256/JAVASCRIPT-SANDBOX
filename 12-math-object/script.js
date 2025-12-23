8beca5e23379ce2a82f55987a0a22609ecec0e18
wang-arshton256 <wangarshton@gmail.com>
2025-12-23 17:38:01 +0300

    math-object

---
+++ 12-math-object/script.js
@@ -0,0 +1,14 @@
+let x;
+x = Math.abs(5); //Absolute
+x = Math.sqrt(500); //Square root
+x = Math.round(4, 2); //Round off
+x = Math.floor(41.7); //Gives the whole number
+x = Math.randon(); // Gives any randon number
+x = Math.randon() * 100; // Gives any randon number between 0 - 100
+x = Math.floor(Math.randon() * 100); //Gives a whole number bewteen 0 - 100
+x = Math.max(4, 6, 10); //Gives the maximum number between the parameters passed
+x = Math.min(4, 6, 10); //Gives the minimum number between the parameters passed
+x = Math.pow(4, 6); //Gives the power of the numbers passed
+
+
+console.log(x);
