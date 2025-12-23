83eb2945c67560fd9818e969f2b7b4e78b7e405b
wang-arshton256 <wangarshton@gmail.com>
2025-12-23 17:36:05 +0300

    type-coercion

---
+++ 08-type-coercion/script.js
@@ -0,0 +1,8 @@
+let x;
+x = 5 + '5';
+
+//console.log(x, typeof x);
+//To convert a string to number before concatination, you use the number function
+
+x = 5 + Number('5');
+console.log(x, typeof x);
