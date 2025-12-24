72da0295712d7abbcb1c087a88b31002eec643cc
wang-arshton256 <wangarshton@gmail.com>
2025-12-24 04:26:10 +0300

    Full example of stacks & heap

---
+++ 05-stack-heap/script2.js
@@ -0,0 +1,27 @@
+let name = 'wang';
+age = 25;
+
+let fullProfile = {
+    nationality : 'Ugandan',
+    placeOfBirth : 'Mulago',
+    sex :'male',
+    Address : 'Bweyogerere',
+    phone : +256779252525,
+
+}
+
+let newName = name;
+newName = 'Wangutusi';
+
+let newFullProfile = fullProfile;
+newFullProfile = {
+    nationality : 'Rwandees',
+    placeOfBirth :'Nakasero',
+    sex : 'male',
+    Address : 'Muyenga',
+    phone : +256779452525,
+
+}
+
+console.log(newName);
+console.log(newFullProfile);
