095eaaae5e5a4d72bd03fc4f65997729a667be07
wang-arshton256 <wangarshton@gmail.com>
2025-12-24 06:29:19 +0300

    added more array properties

--- 02-arrays-and-objects/02-array-methods/script.js
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -1,3 +1,5 @@
+
+let x;
 const arr = [25, 85, 98, 74];
 
 arr[arr.length] = 'mango';
@@ -9,5 +11,8 @@ arr.unshift(99); // Adds 99 to index (0);
 arr.shift(); // Removes first value at index (0)
 arr.reverse(); // Reverses last action made to the array
 
+x = arr.includes(20); // Checks if the array contains the value of 20
+x = arr.indexOf(74) // Checks whats index of 74 is
 
-console.log(arr);
+x = arr.slice(0, 3) //Slice checks for the values beginging from the requested index of (0 - 3)
+console.log(x);
