b68fce6ab9ea0778837f1ec06c1d8640d162730d
wang-arshton256 <wangarshton@gmail.com>
2025-12-27 04:57:39 +0300

    updated challenge 2

--- 02-arrays-and-objects/04-array-challenges/script.js
+++ 02-arrays-and-objects/04-array-challenges/script.js
@@ -8,9 +8,11 @@ const arr = [].concat(numbers).reverse();
 
 console.log(arr);
 
+
 // Challenge 2
 const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [6, 7, 8, 9, 10];
+const arr2 = [5, 6, 7, 8, 9, 10];
+removeFirst = arr2.shift();
 
 const arr3 = arr1.concat(arr2);
 console.log(arr3);
