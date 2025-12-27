9c8727e1d4eed622ae36078cfe809529fa39ab38
wang-arshton256 <wangarshton@gmail.com>
2025-12-28 02:16:27 +0300

    simpler way of creating an object

--- 02-arrays-and-objects/06-object-spread-methods/script.js
+++ 02-arrays-and-objects/06-object-spread-methods/script.js
@@ -8,6 +8,14 @@ shop.contact = +256779262626;
 x = shop;
 console.log(x);
 
+//Simpler way of creating an object
+const obj1 = {};
+obj1.name = 'wang';
+obj1.age = 25;
+obj1.sex = 'male'
+
+console.log(obj1)
+
 
 // More nexting
 const person = {
