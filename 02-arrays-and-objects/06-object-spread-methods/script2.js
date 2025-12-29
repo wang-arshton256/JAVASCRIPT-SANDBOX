a1f1580925188fbbbfdbd1fffb4e2fffe6720edb
wang-arshton256 <wangarshton@gmail.com>
2025-12-30 02:56:26 +0300

    passing objects as an array

--- 02-arrays-and-objects/06-object-spread-methods/script2.js
+++ 02-arrays-and-objects/06-object-spread-methods/script2.js
@@ -9,6 +9,7 @@ x = { ...profile, ...education };
 
 console.log(x)
 
+// Passing objects as an array
 let y;
 const student = [
     { std1: 'Wangutusi', age: 25, sex: 'male', nationality: 'Ugandan', },
@@ -19,30 +20,3 @@ const student = [
 y = student[2];
 console.log(y); 
 
-let q;
-const beneficiaries = {
-    profile: {
-    name: 'wangutusi',
-    age: 25,
-    sex: 'male',
-    nationality: 'Uganda',
-        phone: +256778252525,
-        address: {
-            homeLocation: 'Kirinya',
-            homeDistrict: 'Mbale',
-            office: {
-                mainBranch: 'Muyenga',
-                reginalBranchEastern: 'Jinja',
-                reginalBranchWestern: 'Mbarara',
-                reginalBranchCentral: 'Kampala',
-                reginalBranchNorthern: 'Lira',
-
-            }
-
-        }
-    }
-
-}
-
-
-console.log(beneficiaries.profile.address);
