fb4f777976778a1d1059da43fbe882338946ba01
wang-arshton256 <wangarshton@gmail.com>
2025-12-30 18:02:51 +0300

    capitalze challenge

---
+++ 01-console/10-capitalize-challenge/script.js
@@ -0,0 +1,8 @@
+const myString = 'developer';
+capitalize = myString.charAt(0);
+capitalizeFirst = capitalize.toUpperCase();
+remainingLetters = myString.slice(1);
+myNewString = capitalizeFirst + remainingLetters
+
+
+console.log(myNewString);
