fa8ec034a93a634bb0629ef1704820fa543cb69b
wang-arshton256 <wangarshton@gmail.com>
2025-12-30 04:08:23 +0300

    naming

---
+++ 02-arrays-and-objects/07-destructuring-naming/script.js
@@ -0,0 +1,13 @@
+const firstName = 'John';
+const lastName = 'Doe';
+const age = 30;
+
+// While naming variable with similar values,you can simply add them without adding the value;
+const person = {
+    firstName,
+    lastName,
+    age,
+
+}
+
+console.log(age);
