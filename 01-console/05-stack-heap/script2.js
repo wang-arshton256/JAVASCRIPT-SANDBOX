3e6be56ad0cc6977e47ac0b8e18a8650c37a7738
wang-arshton256 <wangarshton@gmail.com>
2025-12-30 17:59:48 +0300

    stack& heap

---
+++ 01-console/05-stack-heap/script2.js
@@ -0,0 +1,30 @@
+// Data stored in a stack.
+let name = 'wang';
+age = 25;
+
+
+// Data stored in a heap (Is accessed by referencing or pointig to the newly created variable.)
+let fullProfile = {
+    nationality : 'Ugandan',
+    placeOfBirth : 'Mulago',
+    sex :'male',
+    Address : 'Bweyogerere',
+    phone : +256779252525,
+
+}
+
+let newName = name;
+newName = 'Wangutusi';
+
+let newFullProfile = fullProfile;
+newFullProfile = {
+    nationality : 'Rwandees',
+    placeOfBirth :'Nakasero',
+    sex : 'male',
+    Address : 'Muyenga',
+    phone : +256779452525,
+
+}
+
+console.log(newName);
+console.log(newFullProfile);
