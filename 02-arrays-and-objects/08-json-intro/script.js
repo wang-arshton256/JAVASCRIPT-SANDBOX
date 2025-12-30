e4aa67bbe9956ad80f35bbfbaa492974870e6481
wang-arshton256 <wangarshton@gmail.com>
2025-12-30 04:54:32 +0300

    Passing several arrays using JSON

--- 02-arrays-and-objects/08-json-intro/script.js
+++ 02-arrays-and-objects/08-json-intro/script.js
@@ -15,3 +15,21 @@ console.log(post)
 const obj = JSON.parse(str);
 
 console.log(obj)
+
+// Passing several arrays using JSON
+const beneficiaries = [
+
+    {
+        id: 1,
+        title: 'Post One',
+        body: 'This is the body'
+    },
+    {
+        id: 2,
+        title: 'Post two',
+        body: 'This is the second body'
+    }
+];
+
+const newBeneficiaries = JSON.stringify(beneficiaries);
+console.log(newBeneficiaries);
