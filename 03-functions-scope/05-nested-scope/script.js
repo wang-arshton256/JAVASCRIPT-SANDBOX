a719b25c2d85bcc5634b35abab185ab7f9133f65
wang-arshton256 <wangarshton@gmail.com>
2026-01-05 00:00:59 +0300

    nested scope

---
+++ 03-functions-scope/05-nested-scope/script.js
@@ -0,0 +1,27 @@
+function first() {
+    const x = 1000;
+
+    function second() {
+        const y = 5000;
+        const add = y + x;
+        console.log(add);
+        //console.log(x + y);
+
+    }
+    //console.log(y);
+    second();
+}
+
+first();
+
+
+if (true) {
+    const w = 100;
+    
+    if (w === 100) {
+        const t = 150;
+        console.log(w + t);
+    }
+   
+}
+
