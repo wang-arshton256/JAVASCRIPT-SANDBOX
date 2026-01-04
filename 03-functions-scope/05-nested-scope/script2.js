fa4bc702ea4cad089a9e8be205773ef0e621e762
wang-arshton256 <wangarshton@gmail.com>
2026-01-05 00:01:33 +0300

    more nested scope

---
+++ 03-functions-scope/05-nested-scope/script2.js
@@ -0,0 +1,18 @@
+// Parent scope
+function sum() {
+    const sem1 = 350;
+    const sem2 = 452;
+    const year1Results = sem1 + sem2;
+    
+    console.log(year1Results)
+    
+    // Nested child scope
+    function sum2() {
+        const sem3 = 780;
+        const sem4 = 680;
+        const year2Results = sem3 + sem4
+        console.log(year2Results)
+    }
+    sum2();
+}
+sum();
