0711e049a31d5d49b56322df57a341570b1b634f
wang-arshton256 <wangarshton@gmail.com>
2026-01-05 00:55:14 +0300

    declarations-vs-expressions

---
+++ 03-functions-scope/06-declarations-vs-expressions/script.js
@@ -0,0 +1,13 @@
+// Function Declaratipns
+function addDollarSign(value) {
+    return '$' + value;
+}
+console.log(addDollarSign(100));
+
+
+// Function Expressions
+
+const addPlusSign = function (value) {
+    return '+' + value;
+}
+console.log(addPlusSign(100));
