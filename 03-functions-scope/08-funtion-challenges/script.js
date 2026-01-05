bd07e26a4b4d1f86e79d2dd0f9ad646ca32b6b0b
wang-arshton256 <wangarshton@gmail.com>
2026-01-05 03:24:48 +0300

    a function that takes temperature in fahrenheit as an argument and returns the temperature in celsius

---
+++ 03-functions-scope/08-funtion-challenges/script.js
@@ -0,0 +1,8 @@
+// Chalenge 1
+// function getCelsius(f) {
+//    const celsius = ((f - 32) * 5) / 9;
+//    return celsius;
+//}
+
+const getCelsius = (f) => ((f - 50) * 5) / 9;
+console.log(`The temp is ${getCelsius(32)}\xB0C`)
