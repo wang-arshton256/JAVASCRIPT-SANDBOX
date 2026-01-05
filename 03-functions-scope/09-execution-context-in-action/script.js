490e258db531aaad4ad78599ad37949d543e0614
wang-arshton256 <wangarshton@gmail.com>
2026-01-05 05:31:21 +0300

    execution context

---
+++ 03-functions-scope/09-execution-context-in-action/script.js
@@ -0,0 +1,11 @@
+let x = 200;
+let y = 150;
+
+function addNumbers(num1, num2) {
+    sum = num1 + num2;
+    return sum;
+
+}
+let sum1 = addNumbers (x, y);
+let sum2 = addNumbers(700, 150);
+ console.log(sum1, sum2);
