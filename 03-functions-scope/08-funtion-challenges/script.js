0b641bf9b454424a28c89c3797b3d6ac2ee622f6
wang-arshton256 <wangarshton@gmail.com>
2026-01-05 03:40:46 +0300

    challenge 2

--- 03-functions-scope/08-funtion-challenges/script.js
+++ 03-functions-scope/08-funtion-challenges/script.js
@@ -1,4 +1,6 @@
-// Chalenge 1
+// Chalenge 1 Create a function called a getCelsuis() that 
+// takes temperature in fahrenheit as an argument and returns the temperature in celsius
+
 // function getCelsius(f) {
 //    const celsius = ((f - 32) * 5) / 9;
 //    return celsius;
@@ -6,3 +8,17 @@
 
 const getCelsius = (f) => ((f - 50) * 5) / 9;
 console.log(`The temp is ${getCelsius(32)}\xB0C`)
+
+
+// Challenge 2
+// create an arrow function called minMax() which takes in an
+// array of numbers and returns an object with the minimum and maximum numbers in the array.
+
+function minMax(arr) {
+    const min = Math.min(arr);
+    console.log(min);
+}
+minMax(1, 2, 3, 4, 7);
+
+
+
