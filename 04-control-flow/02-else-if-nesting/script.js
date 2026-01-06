7d34e3f84df2efde41c54676bcb0e3f18cc9e378
wang-arshton256 <wangarshton@gmail.com>
2026-01-06 05:41:22 +0300

    nested if esle statements

--- 04-control-flow/02-else-if-nesting/script.js
+++ 04-control-flow/02-else-if-nesting/script.js
@@ -1,5 +1,5 @@
 
-const d = new Date(10, 30, 2022, 13, 0, 0);
+const d = new Date(10, 30, 2022, 22, 0, 0);
 const hour = d.getHours();
 
 if (hour < 12) {
@@ -10,4 +10,26 @@ if (hour < 12) {
     
 else {
     console.log('It is night.');
+}
+
+
+
+// Nested if
+
+if (hour < 12) {
+    console.log('Good morning');
+
+    if (hour === 6) {
+        console.log('Wake up');
+    }
+} else if (hour < 18) {
+    console.log('It is afternoon');
+}
+    
+else {
+    console.log('It is night.');
+
+    if (hour > 20) {
+        console.log('Time to sleep!!!')
+    }
 }
