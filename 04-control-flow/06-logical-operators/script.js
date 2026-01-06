7b44a0617cc862585cc01e135672a8573e63c5e5
wang-arshton256 <wangarshton@gmail.com>
2026-01-06 12:41:21 +0300

    logical operators

---
+++ 04-control-flow/06-logical-operators/script.js
@@ -0,0 +1,36 @@
+console.log(10 < 20 && 30 > 15 && 40 > 30);
+console.log(10 > 20 || 30 < 15);
+
+// && - will return first false value or the last value
+let a;
+
+a = 10 && 20;
+a = 10 && 20 && 30;
+a = 10 && 0 && 30;
+a = 10 && '' && 0 && 30;
+
+console.log(a);
+
+const posts = ['Post One', 'Post Two'];
+posts.length > 0 && console.log(posts[0]); // use .length method to check for properties in array and not live undefined if array isempty
+
+// || - will return the first truthy value if the last value
+
+let b;
+
+b = 10 || 20;
+b = 0 || 20;
+b = 0 || null || '' || undefined;
+
+console.log(b);
+
+// ?? - Returns the right side operand when the left is null or undefined
+let c;
+
+c = 10 ?? 20;
+c = null ?? 20;
+c = undefined ?? 30;
+c = 0 ?? 30;
+c = '' ?? 30; 
+
+console.log(c);
