a9bec58dae247642b2fcb48402aab66ceda8b147
wang-arshton256 <wangarshton@gmail.com>
2026-01-19 09:36:02 +0300

    keyboard events

---
+++ 07-events/05-keyboard-events copy/event-keycodes/script.js
@@ -0,0 +1,22 @@
+// Method  
+window.addEventListener('keydown', (e) => {
+    const insert = document.getElementById('insert');
+
+    insert.innerHTML = `
+    
+   <div class="key">
+      ${e.key === '' ? 'Space' : e.key}
+    <small>e.key</small>
+    </div>
+
+    <div class="key">
+      ${e.keyCode}
+      <small>e.keyCode</small>
+    </div>
+    <div class = "key">
+        ${e.code}
+        <small>e.code</small>
+    </div> 
+    
+    `;
+});
