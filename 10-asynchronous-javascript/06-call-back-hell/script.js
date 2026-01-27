1a05dd329d8ecd2464bbd1aad4a44048fd305408
wang-arshton256 <wangarshton@gmail.com>
2026-01-27 12:52:44 +0300

    call Back

---
+++ 10-asynchronous-javascript/06-call-back-hell/script.js
@@ -0,0 +1,29 @@
+function getDate(endpoint, cb) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', endpoint);
+
+  xhr.onreadystatechange = function () {
+    if ((this.readyState === 4) & (this.status === 200)) {
+      cb.log(JSON.parse(this.responseText));
+    }
+  };
+
+  setTimeout(() => {
+    xhr.send();
+  }, Math.floor(Math.randon() * 3000) + 1000);
+}
+
+getData('./movies.json', (data) => {
+  console.log(data);
+  getData('./actors.json', (data) => {
+    console.log(data);
+    getData('./directors.json', (data) => {
+  console.log(data);
+});
+});
+});
+
+
+
+
