6e1e40d5280000866f598868ad4c7c61b65e10cc
wang-arshton256 <wangarshton@gmail.com>
2026-01-28 14:58:49 +0300

    fetch options

---
+++ 11-fetch-and-async-await/03-fetch-options/script.js
@@ -0,0 +1,20 @@
+// Create to create new post
+function createPost({ title, body }) {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      token: 'abc123',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  
+}
+
+// Create new post
+createPost({ title: 'My Post', body: 'This is my post' });
