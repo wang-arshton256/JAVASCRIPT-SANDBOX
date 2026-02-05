42c604b96cb09cd168832ca12e786c16c3d415dc
wang-arshton256 <wangarshton@gmail.com>
2026-02-05 13:54:05 +0300

    map position using  leaflet API

--- 13-web-and-browser-api/02-map-position/script.js
+++ 13-web-and-browser-api/02-map-position/script.js
@@ -4,3 +4,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Add marker
+const marker = L.marker[0, 0].addTo(map);
+
+// Change marker Coordinated to current position
+navigator.geolocation.getCurrentPosition(function (pos) {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+
+    // Set Latitude & Longitude
+    marker.setLatLng([lat, lng].update());
+
+    // Set View
+    map.setView([lat, lng], 13);
+
+    //Add popup
+    marker.bindPopup('<strong>Hello World</strong> <br> This is my location');
+
+})
