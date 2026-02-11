28dcea90eb2382931505762fb04610446b5dd807
wang-arshton256 <wangarshton@gmail.com>
2026-02-11 14:07:45 +0300

    prototypes

--- 14-oop-constructors-/05-prototypes/script.js
+++ 14-oop-constructors-/05-prototypes/script.js
@@ -2,10 +2,37 @@ function Rectangle(name, width, length) {
     this.name = name;
     this.width = width;
     this.height = length;
-    this.area = function () {
+}
+
+// Addding methods to the prototype
+Rectangle.prototype.area = function () {
         return this.length * this.width;
     };
+
+Rectangle.prototype.perimeter = function () {   
+    return 2 * (this.width + this.length);
+}
+
+Rectangle.prototype.isSquare = function () {   
+    return 2 * (this.width + this.length);
+}
+
+Rectangle.prototype.isSquare = function () {   
+    return this.width === this.length;
+}
+
+Rectangle.prototype.changeName = function () {   
+    return this.name === newName;
 }
 
 const rect = new Rectangle('Rect', 47, 63);
-console.log(Object.getPrototypeOf(rect));
+const rect2 = new Rectangle('Rect 2', 30, 40);
+
+console.log(rect);
+console.log(rect.area());
+console.log(rect.perimeter());
+console.log(rect.isSquare());
+rect.changeName('Test');
+console.log(rect.name);
+
+console.log(rect2.area());
