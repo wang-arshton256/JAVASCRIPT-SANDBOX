4b400bb28bc207d2d2866e3424c10838b846bb37
wang-arshton256 <wangarshton@gmail.com>
2026-02-13 11:50:09 +0300

    updated getters & setters

--- 14-oop-classes-private-property/04-getters-setters-classes/script.js
+++ 14-oop-classes-private-property/04-getters-setters-classes/script.js
@@ -19,14 +19,22 @@ class Person {
         this._lastName = this.capitalizeFirst(value);
     }
 
+    // Getter
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+}
+
     capitalizeFirst(value) {
         return value.charAt(0).toUpperCase() + value.slice(1);
     }
 }
 
 const person1 = new Person('Wang', 'Arshton');
-console.log(person1.firstName)
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.fullName);
 
 person1.firstName = 'derick';
+person1.lastName = 'smith';
 console.log(person1);
 
